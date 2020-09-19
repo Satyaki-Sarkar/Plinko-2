389f7112f895c117c2300f831e848bcb2e680f2a
Satyaki <satyakisarkar7500@gmail.com>
2020-09-19 17:01:19 +0530

    C33 Activity

--- sketch.js
+++ sketch.js
@@ -93,7 +93,7 @@ function draw() {
           {
             score=score+500;
             particles=undefined;
-          } else if(particles.body.position.x<560)
+          } else if(particles.body.position.x>320 && particles.body.position.x<560)
           {
             score=score+100;
             particles=undefined;
@@ -111,16 +111,29 @@ function draw() {
   } else if (gameState==="END")
   {
     textSize(60);
-    text("Score : "+score,400,400);
+    text("Score : "+score,100,400);
     textSize(20);
-    text("Press SPACE to restart.",400,600);
+    text("Press SPACE to restart.",200,600);
     if(keyIsDown(32))
     {
       gameState="PLAY";
       turn=1;
       score=0;
     }
-  } 
+  }
+  textSize(30);
+  if (gameState==="PLAY"){
+    for(var a=15;a<=255;a+=80){
+      text("500",a,520);
+    }
+    for(var a=335;a<=495;a+=80){
+      text("100",a,520);
+    }
+    for(var a=575;a<=735;a+=80){
+      text("200",a,520);
+    }
+  }
+ 
 }
 function mousePressed(){
   if(turn<=5)
